e8a4eff7fb913109bb63b326f9c1f1fc59f58454
heatze <richy.sting@gmail.com>
2014-01-15 14:57:24 +0100

    code

---
+++ hack/quell1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "quell2.h"
+
+
+int main(){
+	int i = 12;
+	pr(i);
+	return 0;
+
+
+
+
+}
---
+++ hack/readFile.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void){
+	FILE *fp = fopen("test.txt", "r+");
+	if(fp == NULL)
+		printf("\nERROR\n");
+	else{
+		char str[100];
+	       	fgets( str, 100, fp);
+		printf("\n%s\n", str);
+		printf("\n worked well\n %d \n" , fgetc(fp) );
+		fputs("hallo\n", fp);
+		
+	}	
+		
+	return 0;
+
+}
+
