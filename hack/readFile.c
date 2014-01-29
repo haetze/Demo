b47729fa69015f34301caa86fcf97b7b64e3fab2
Richard <richy.sting@googlemail.com>
2014-01-29 10:41:05 +0100

    Update readFile.c

--- hack/readFile.c
+++ hack/readFile.c
@@ -3,19 +3,17 @@
 
 
 int main(void){
-	FILE *fp = fopen("test.txt", "r+");
-	if(fp == NULL)
-		printf("\nERROR\n");
-	else{
-		char str[100];
-	       	fgets( str, 100, fp);
-		printf("\n%s\n", str);
-		printf("\n worked well\n %d \n" , fgetc(fp) );
-		fputs("hallo\n", fp);
-		
-	}	
-		
-	return 0;
-
+        FILE *fp = fopen("test", "rw+"); // for unix test.txt
+        if(fp == NULL)
+                printf("\nERROR\n");
+        else{
+                char str[100];
+                fgets( str, 100, fp);
+                printf("\n%s\n", str);
+                printf("\nworked well\n%d \n" , fgetc(fp) );
+                fputs("hallo\n", fp);
+                
+        }  
+        system("PAUSE");	
+        return 0;
 }
-
