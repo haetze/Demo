51b2647e1fdb8183b8a3a6bb660ff58ec9432980
haetze <richy.sting@gmail.com>
2014-07-11 20:36:45 +0200

    -a

--- hack/testforAnother.c
+++ hack/testforAnother.c
@@ -9,7 +9,7 @@ int main(int argc, char * argv[])
     /*Spawn a child to run the program.*/
     char *argv2[]={"dart"};
     for (int i  = 1; i<sizeof(argv); i++){
-		    argv2[i] = argv[i]
+		    argv2[i] = argv[i];
     }	
     execv( "/Applications/dart/dart-sdk/bin/dart" ,argv2);
     return 0;
