371273f622348632d785770824ca0e16222b1191
haetze <richy.sting@gmail.com>
2014-07-11 20:36:12 +0200

    -a

---
+++ test2/test2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test2
+//
+//  Created by Richard Stewing on 11.07.14.
+//  Copyright (c) 2014 Richard Stewing. All rights reserved.
+//
+
+#include <CoreServices/CoreServices.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
