afb8530853c4ac63cdd6f822951e7ad6bb2d987f
haetze <richy.sting@gmail.com>
2014-12-29 09:35:22 -0600

    pointer test

--- hack/pointer.c
+++ hack/pointer.c
@@ -3,15 +3,19 @@
 void increment(int *n);
 void increment2(int n);
 
-int main(){
-	int a = 4;
-	int *b = &a;
-	printf("Momentan ist a = %d \n jetzt wird a als erstes ohne pointer incrementiert\n", a);
-	increment2(a);
-	printf("a ist immer noch %d\n jetzt mit pointer", a);
-	increment(b);
-	printf("\n\na ist jetzt %d\n", a);		
+typedef struct Types {
+	int n;
+	int m;
+}Type;
 
+void sq(Type * n);
+
+
+int main(){
+	Type a ={12, 12};
+	Type *b = &a;
+	sq(b);
+	printf("%d", (b)->n);
 	return 0;
 
 
@@ -21,13 +25,13 @@ int main(){
 
 
 void increment(int *n){
-
 	++(*n);
 }
 
 void increment2(int n){
-
-
 	++n;
+}
 
+void sq(Type * n){
+	n->n = 123; 
 }
